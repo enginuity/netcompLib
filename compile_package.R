6e36076fb78dccea6eb031ce47cbad0dcc72a041
Lawrence Wang <625lawrence@gmail.com>
2014-12-10 00:10:27 -0500

    add loading scripts and update compile script

--- compile_package.R
+++ compile_package.R
@@ -1,20 +1,24 @@
-## Contains code for testing and compiling package.
-
-library(testthat)
-
-## Run tests
-
-library(metacode)
-library(stringr)
-
-library(roxygen2)
-
-update_fx_documentation(FD = FilesDescription(dirlist = "netcomplib/"), fill_emptyparam = FALSE)
-roxygenise("netcomplib/", clean = TRUE)
+##@S Contains code to compile the netcomp_sbm package (to be run from R)
 
 
+# Optional Steps ----------------------------------------------------------
+## These only need to be run, if the source code was edited. 
 
-## ONLY IF PASSES TEST -> INSTALL PACKAGE
+## Load up needed libraries
+require(metacode) ## This is a package I've written to speed up my own coding
+require(stringr)
 
-system("R CMD INSTALL netcomplib")
+## Update documentation -- this looks for new parameters & 
+update_fx_documentation(FD = FilesDescription(dirlist = "netcomp_lib/R/"), fill_emptyparam = FALSE)
+
+
+# Compile Package ---------------------------------------------------------
+## Both these steps need to be run in order to build the package properly
+
+## Generate the documentation -- THIS MUST be run before building packages (since the documentation files are not version-controlled, as the version-controlled version is in the raw source code)
+require(roxygen2)
+roxygenise("netcomp_lib/", clean = TRUE)
+
+## Install the package
+system("R CMD INSTALL netcomp_lib")
 
