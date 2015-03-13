0263cfe951f93e08d4bd620b6c58cd0e8ed09f9b
Lawrence Wang <625lawrence@gmail.com>
2015-03-12 20:17:22 -0400

    add latent space model generation code

--- netcompLib/R/NetworkModelLSM.R
+++ netcompLib/R/NetworkModelLSM.R
@@ -1,5 +1,5 @@
 # Defines the NetworkModelLSM class
-setClass("NetworkModelLSM", representation(assign = "numeric", probmat = "matrix"), contains = "NetworkModel")
+setClass("NetworkModelLSM", representation(locs = "matrix", alpha = "numeric"), contains = "NetworkModel")
 
 #' Constructor for LSM network model
 #' 
@@ -13,7 +13,33 @@ setClass("NetworkModelLSM", representation(assign = "numeric", probmat = "matrix
 #' @export
 #' 
 NetworkModelLSM = function(Nnodes = 10, model_param = set_model_param()) {
-  ## TODO: - fill this in eventually 
+  
+  K = model_param$latent_nclass
+  D = model_param$latent_dim
+  gen_norm = model_param$latent_isgennorm
+  sd_center = model_param$latent_sdcenter
+  ## TODO: [TEMP] REMOVE these variables. 
+  
+  # K,gen_norm = TRUE, D = 2, sd_center = 5) {
+  
+  group_assign = sample(1:K, size = Nnodes, replace = TRUE)
+  centers = list()
+  
+  for(j in 1:K) {
+    if (gen_norm) {
+      centers[[j]] = rnorm(n = D, mean = 0, sd = sd_center)
+    } else {
+      centers[[j]] = runif(n = D, min = -2 * sd_center, max = 2 * sd_center)
+    }
+  }
+  
+  locs = matrix(nrow= Nnodes, ncol = D)
+  for(j in 1:Nnodes) {
+    locs[j,] = centers[[group_assign[j]]] + rnorm(n = D, mean = 0, sd = 1)
+  }
+  
+  netm = new("NetworkModelLSM", Nnodes = Nnodes, locs = locs, alpha = runif(1,0,10))
+  return(netm)
 }
 
 
@@ -40,9 +66,12 @@ setMethod("getNetType", signature(NetM = "NetworkModelLSM"), getNetType.NetworkM
 #' @export
 #' 
 getEdgeProbMat.NetworkModelLSM = function(NetM) {
-  ## TODO - fix this
-  
-  return(res)
+  Nnodes = getNnodes(NetM)
+  odds = exp(NetM@alpha - dist(NetM@locs))
+  prob_mat = matrix(0, nrow = Nnodes, ncol = Nnodes)
+  prob_mat[lower.tri(prob_mat)] = odds / (1 + odds)
+  prob_mat = prob_mat + t(prob_mat)
+  return(prob_mat) 
 }
 setMethod("getEdgeProbMat", signature = (NetM = "NetworkModelLSM"), getEdgeProbMat.NetworkModelLSM)
 
