df727ec5b9df6262ee99628b762f550d3e77eaf4
Lawrence Wang <625lawrence@gmail.com>
2015-03-13 21:43:41 -0400

    add function to set simulation parameters

--- netcompLib/R/parameter_lists.R
+++ netcompLib/R/parameter_lists.R
@@ -26,3 +26,15 @@
 set_model_param = function(pmin = 0, pmax = 1, block_nclass = 3, block_avgdensity = NULL, block_assign = NULL, block_probs = NULL, random_ngroups = 10, tree_type = "random", latent_dim = 3, latent_nclass = 3, latent_sdcenter = 5, latent_isgennorm = TRUE) {
   return(list(pmin = pmin, pmax = pmax, block_nclass = block_nclass, block_avgdensity = block_avgdensity, block_assign = block_assign, block_probs = block_probs, random_ngroups = random_ngroups, tree_type = tree_type, latent_dim = latent_dim, latent_nclass = latent_nclass, latent_sdcenter = latent_sdcenter, latent_isgennorm = latent_isgennorm))
 }
+
+set_sim_param = function(cc_adj = c(0,2), thres_ignore = c(5, 10), alphas = 0.05, n_models = c(1,25,50,100)) {
+  
+  ## pl is a list of parameters, and the constitutient values could be vectors if more than one is to be tested. This returns a matrix of pvalues
+  ## -- thres_ignore: Ignore all cells concern a smaller number of edges than this value. Set to 0 to use every single cell. 
+  ## -- cc_adj: The amount of SE's away from correlation estimate used.
+  ## -- alphas: The cutoffs for p-values to be used
+  ## -- -- this is set to 2 by default; setting to 0 -> simply use correlation estimate (thresholded to 0)
+  return(list(cc_adj = cc_adj, thres_ignore = thres_ignore, alphas = alphas, n_models = n_models))
+}
+
+  
