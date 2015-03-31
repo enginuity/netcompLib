666877206a038af534e09f48c09d6e991b1a1588
Lawrence Wang <625lawrence@gmail.com>
2015-03-31 15:26:43 -0400

    add baseline description for new tests

---
+++ test_asymptotics.R
@@ -0,0 +1,14 @@
+
+## Want to test the following asymptotics (for each method)
+
+# how close to a chi-square distribution is the raw test statistic? how does this change as network size increases? -- can also increase the number of observed networks -- this is to only test to see if everything becomes really optimal. 
+
+# does this still work when we take a random subset of edges? presumably... 
+
+# maybe this requires a function that can compute the correct df given two models.
+
+
+
+
+
+
