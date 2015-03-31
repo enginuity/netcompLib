31eea68a3de0e496d410329bf6151a7b8d0ab99f
Lawrence Wang <625lawrence@gmail.com>
2015-03-31 19:05:25 -0400

    write function that computes the likelihood of any network model given adjacency matrices

---
+++ netcompLib/R/generic_computeLik.R
@@ -0,0 +1,31 @@
+##@S Generic function that performs the hypothesis test (computes the p-value for the likelihood ratio test)
+
+setGeneric("computeLik", function(NetM, adja, loglik) standardGeneric("computeLik"))
+
+computeLik = function(NetM, adja, loglik = TRUE) {
+  stop("Placeholder for generic function -- this call is meaningless for a generic NetworkModel")
+}
+
+computeLik.NetworkModel = function(NetM, adja, loglik = TRUE) {
+  if (length(dim(adja)) == 2) {
+    adjm = adja; Nobs = 1
+  } else if (length(dim(adja)) == 3) {
+    adjm = apply(adja, c(1,2), sum); Nobs = dim(adja)[3]
+  } else {
+    stop("Invalid input 'adja' (not a 2D or 3D array)")
+  }
+  
+  eps = getEdgeProbMat(NetM)
+  resm = adjm * log(eps) + (Nobs - adjm) * log(1 - eps)
+  res = sum(adjm[upper.tri(x = resm, diag = FALSE)])
+    
+  if (loglik) {
+    return(res) 
+  } else {
+    return(exp(res))
+  }
+}
+
+# setMethod ---------------------------------------------------------------
+setMethod("computeLik", signature(NetM = "NetworkModel"), computeLik.NetworkModel)
+
