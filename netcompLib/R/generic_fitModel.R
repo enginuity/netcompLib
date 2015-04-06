cf724a986cdb8830f05e0a2818edcc6a3ff775df
Lawrence Wang <625lawrence@gmail.com>
2015-04-06 05:09:48 -0400

    add documentation

--- netcompLib/R/generic_fitModel.R
+++ netcompLib/R/generic_fitModel.R
@@ -4,7 +4,16 @@ setGeneric("fitModel", function(NetS, adja) standardGeneric("fitModel"))
 
 ## TODO: Figure out why roxygen documentation doesn't seem to work ?!?!?! it behaves differently for whatever reason. there's something strange with this file ???
 
-
+## TODO: [Documentation-AUTO] Check/fix Roxygen2 Documentation (fitModel)
+#' Estimates the best-fit model for given structure
+#' 
+#' @param NetS NetworkStruct object
+#' @param adja temp
+#' 
+#' @return NetworkModel object containing best-fit model per structure. 
+#' 
+#' @export
+#' 
 fitModel = function(NetS, adja) {
   # if adja is null then fill in random uniform probabilities. 
   stop("No implementation in template")
@@ -12,16 +21,46 @@ fitModel = function(NetS, adja) {
 }
 
 
+## TODO: [Documentation-AUTO] Check/fix Roxygen2 Documentation (fitModel.NetworkStruct)
+#' <What does this function do>
+#' 
+#' @param NetS temp
+#' @param adja temp
+#' 
+#' @return temp
+#' 
+#' @export
+#' 
 fitModel.NetworkStruct = function(NetS, adja) { 
   stop("No implmentation for template NetworkStruct")
 }
 
 
+## TODO: [Documentation-AUTO] Check/fix Roxygen2 Documentation (fitModel.NetworkStructList)
+#' <What does this function do>
+#' 
+#' @param NetS temp
+#' @param adja temp
+#' 
+#' @return temp
+#' 
+#' @export
+#' 
 fitModel.NetworkStructList = function(NetS, adja) {
   return(lapply(NetS@models, function(x) { fitModel(x, adja) } ))
 }
 
 
+## TODO: [Documentation-AUTO] Check/fix Roxygen2 Documentation (fitModel.NetworkStructSBM)
+#' <What does this function do>
+#' 
+#' @param NetS temp
+#' @param adja temp
+#' 
+#' @return temp
+#' 
+#' @export
+#' 
 fitModel.NetworkStructSBM = function(NetS, adja) {
   res = NetworkModel(Nnodes = getNnodes(NetS), type = "block", model_param = set_model_param(block_assign = NetS@groups))
   if (is.null(adja)) {
@@ -33,6 +72,16 @@ fitModel.NetworkStructSBM = function(NetS, adja) {
 }
 
 
+## TODO: [Documentation-AUTO] Check/fix Roxygen2 Documentation (fitModel.NetworkStructRND)
+#' <What does this function do>
+#' 
+#' @param NetS temp
+#' @param adja temp
+#' 
+#' @return temp
+#' 
+#' @export
+#' 
 fitModel.NetworkStructRND = function(NetS, adja) {
   if (is.null(adja)) {
     stop("Not implemented")
@@ -44,6 +93,16 @@ fitModel.NetworkStructRND = function(NetS, adja) {
 }
 
 
+## TODO: [Documentation-AUTO] Check/fix Roxygen2 Documentation (fitModel.NetworkStructHRG)
+#' <What does this function do>
+#' 
+#' @param NetS temp
+#' @param adja temp
+#' 
+#' @return temp
+#' 
+#' @export
+#' 
 fitModel.NetworkStructHRG = function(NetS, adja) {
   if (is.null(adja)) {
     stop("Not implemented")
