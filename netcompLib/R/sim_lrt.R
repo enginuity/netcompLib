65b480f1c3ea8dd5de1175330ad121ad4ec8989a
Lawrence Wang <625lawrence@gmail.com>
2015-05-01 16:05:22 -0400

    delete function sim_one_set

--- netcompLib/R/sim_lrt.R
+++ netcompLib/R/sim_lrt.R
@@ -21,36 +21,3 @@ sim_mle_loglik_ratio = function(btree, ftree, Nsim = 1) {
   return(-2 * (sameparam - diffparam))
 }
 
-
-#' Wrapper for compute_pval_multtrees, runs this for a random adjmat. Uses global variables. 
-#' 
-#' Requires existence of: fl_LIST, params, gen_model
-#' where params is a list with: mode, cc_adj, thres_ignore, alphas, n_models, gen_model
-#' -- gen_model is a list with: $mode, $m1, $m2
-#' 
-#' @param noparam DELETE THIS PARAMTER once i fix the auto param generation code. 
-#' 
-#' @return p-value matrix/array
-#' 
-#' @export
-#' 
-sim_one_set = function(noparam) {
-
-  if (gen_model$mode == "tree") {
-    adjmat = abind(cmn_network(gen_model$m1, 1), cmn_network(gen_model$m2,1))
-  } else if (gen_model$mode == "block" | gen_model$mode == "blockmodel") {
-    adjmat = abind(block_network(gen_model$m1, 1), block_network(gen_model$m2, 1))
-  } else if (gen_model$mode == "latent") {
-    adjmat = abind(latent_network(gen_model$m1, 1), latent_network(gen_model$m2, 1))
-  }
-  
-  res = compute_pval_multtrees(
-    adj1 = adjmat[,,1,drop = FALSE], adj2 = adjmat[,,2,drop = FALSE],
-    mode = params$mode,
-    pl = list(thres_ignore = params$thres_ignore,
-              cc_adj = params$cc_adj, alphas = params$alphas),
-    fl_list = fl_LIST, n_models = params$n_models)
-  
-  return(res)
-}
-
