b7b15b839ad7aca98112bec3e72776d9a7784641
Lawrence Wang <625lawrence@gmail.com>
2015-07-21 15:01:02 -0400

    fix computeDfAdj function

--- netcompLib/R/network_functions.R
+++ netcompLib/R/network_functions.R
@@ -1,21 +1,20 @@
 ##@S This is a collection of functions that aid with network comparisons.
 ##@S These are NOT generic functions, but they DO use NetM/NetS's as arguments. 
 
-## TODO: [Documentation-AUTO] Check/fix Roxygen2 Documentation (computeDfAdj)
-## TODO: [Documentation-AUTO] Check/fix Roxygen2 Documentation (computeDfAdj)
-#' Function to compute the theoretical df adjustment
+
+#' Compute Theoretical df Adjustment
 #' 
-#' @param NetM Given NetworkModel object
-#' @param NetS Given NetworkStruct object
-#' @param hidden_nodes ewefwf
+#' For a fixed null hypothesis model, and for a fixed network structure, compute the true theoretical df adjustment
 #' 
-#' @return temp
+#' @param NetM [\code{\link{NetworkModel}}] :: Underlying null model
+#' @param NetS [\code{\link{NetworkStruct}}] :: Testing model structure
+#' @param hidden_edges [matrix-logical] :: Entries of matrix are 'true' if they are hidden in the inference process. (so default would be a matrix of falses)
+#' 
+#' @return A vector of theoretical df adjustments (one for each edge group)
 #' 
 #' @export
 #' 
-computeDfAdj = function(NetM, NetS, hidden_nodes = NULL) {
-  # hidden_nodes should be a logical matrix, with TRUE identifying edges that are hidden.
-  ## TODO: [Dumb Variable Name] -- rename hidden_nodes with hidden_edges !?!??!?!
+computeDfAdj = function(NetM, NetS, hidden_edges = NULL) {
   if (FALSE) {
     ## Test code
     NetM = NetworkModel(Nnodes = 20, type = "block", model_param = set_model_param(block_assign = rep(c(1,2), each = 10), block_probs = matrix(c(.3, .3, .3, .7), nrow = 2)))
@@ -28,7 +27,7 @@ computeDfAdj = function(NetM, NetS, hidden_nodes = NULL) {
   fit_nodeprobs = getEdgeProbMat(NetM, mode = "prob")
   test_nodeids[lower.tri(test_nodeids, diag = TRUE)] = 0
   
-  if (!is.null(hidden_nodes)) { test_nodeids[hidden_nodes] = 0 }
+  if (!is.null(hidden_edges)) { test_nodeids[hidden_edges] = 0 }
   
   ids = unique(as.vector(test_nodeids))
   ids = ids[ids > 0]
@@ -43,3 +42,8 @@ computeDfAdj = function(NetM, NetS, hidden_nodes = NULL) {
   return(1 - cors)
 }
 
+
+
+computeDist = function(x, y = NULL) {
+  
+}
