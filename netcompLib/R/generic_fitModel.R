9f489cdad30eef560a79edd470798114bfe13aea
Lawrence Wang <625lawrence@gmail.com>
2015-09-10 06:23:13 -0400

    add gradient functions to fitting

--- netcompLib/R/generic_fitModel.R
+++ netcompLib/R/generic_fitModel.R
@@ -90,6 +90,15 @@ fitModel.NetworkStructSBM = function(NetS, adja, mode = "default", optim_tries =
   }
   
   if (mode == "corr-global-null") {
+    sample_start = function(n) { 
+      t = rnorm(n)
+      p = 1/(1 + exp(-t[-1]))
+      rmin = max(-c(p/(1-p), (1-p)/p))
+      r = runif(1, min = rmin, max = 1) * .8
+      t[1] = log((1+r)/(1-r))
+      return(t)
+    }
+    
     a1 = adja[,,1]; a2 = adja[,,2]; diag(a1) = -1; diag(a2) = -1
     llfx = function(t,C,n) {
       # C = cbind(c11, c10, c01, c00); n = vector of total counts; t = c(rho, p)
@@ -107,7 +116,22 @@ fitModel.NetworkStructSBM = function(NetS, adja, mode = "default", optim_tries =
       res = c11 * log(p11) + c00 * log(p00) + (c10 + c01) * log(p10)
       return(sum(res))
     }
-    
+    grfx = function(t, C, n) {
+      p = 1/(1 + exp(-t[-1]))
+      r = 2 / (1 + exp(-t[1])) - 1 
+      q = 1 - p; pq = p * q
+      
+      p11 = p^2 + r * pq
+      p00 = q^2 + r * pq
+      p10 = (1-r) * pq
+      
+      c11 = C[,1]; c10 = C[,2]; c01 = C[,3]; c00 = C[,4]
+      res = t * 0
+      
+      res[1] = sum(c11*pq/p11 + c00*pq/p00 - (c10+c01)*pq/p10)
+      res[-1] = c11*(2*p+r*(1-2*p))/p11 + c00*(-2*(1-p)+r*(1-2*p))/p00 + (c10*c01)*(1-2*p)/(p*q)
+      return(res/1000)
+    }
     c11m = matrix(0, NG, NG); c00m = matrix(0, NG, NG)
     c10m = matrix(0, NG, NG); c01m = matrix(0, NG, NG)
     tm = matrix(0, NG, NG)
@@ -135,18 +159,29 @@ fitModel.NetworkStructSBM = function(NetS, adja, mode = "default", optim_tries =
     n = tm[lower.tri(tm, diag = TRUE)]
     
     bestval = -Inf
+    v = rep(1:optim_tries)
     for(i in 1:optim_tries) { 
-      temp = optim(rnorm(length(n) + 1), fn = llfx, C = C, n=n, control = list(fnscale = -1))
+      print(i)
+      temp = optim(sample_start(length(n) + 1), fn = llfx, gr = grfx, C = C, n=n, control = list(fnscale = -1), method = "BFGS")
       if (temp$value > bestval) {
         bestval = temp$value; best = temp
       }
+      v[i] = temp$value
     }
     
-    return(list(res, temp))
+    return(list(res, best, v))
   }
   
   
   if (mode == "corr-global") {
+    sample_start = function(n) { 
+      t = rnorm(n)
+      p = 1/(1 + exp(-t[-1]))
+      r = 0
+      t[1] = log((1+r)/(1-r))
+      return(t)
+    }
+    
     a1 = adja[,,1]; a2 = adja[,,2]; diag(a1) = -1; diag(a2) = -1
     llfx = function(t,D,n) {
       # D = cbind(c11, c10, c01, c00); n = vector of total counts; t = c(rho, p)
@@ -162,10 +197,34 @@ fitModel.NetworkStructSBM = function(NetS, adja, mode = "default", optim_tries =
       p10 = p1 * (1-p2) - A
       p01 = (1-p1) * p2 - A
       p00 = (1-p1)*(1-p2) + A
-      
       if (!all(is_prob(c(p11, p10, p01, p00)))) return(10^-239)
       res = c11 * log(p11) + c00 * log(p00) + c10 * log(p01) + c01 * log(p00)
       return(sum(res))
+    }
+    grfx = function(t, D, n) {
+      # D = cbind(c11, c10, c01, c00); n = vector of total counts; t = c(rho, p)
+      # p1,p2,r in logit form
+      N = nrow(D)
+      r = 2 / (1 + exp(-t[1])) - 1; rt = t[-1]
+      p1 = 1/(1 + exp(-t[seq_len(N)])); rt = rt[-seq_len(N)]
+      p2 = 1/(1 + exp(-rt))
+      q1 = 1 - p1; q2 = 1 - p2
+      c11 = D[,1]; c10 = D[,2]; c01 = D[,3]; c00 = D[,4]
+      s = sqrt(p1 * p2 * q1 * q2)
+      A = r * s
+      dp1 = (1-2*p1)*p2*q2/(2*s)
+      dp2 = (1-2*p2)*p1*q1/(2*s)
+      
+      p11 = p1 * p2 + A
+      p10 = p1 * q2 - A
+      p01 = q1 * p2 - A
+      p00 = q1 * q2 + A
+      
+      res = t * 0
+      res[1] = sum(c11*s/p11 + c00*s/p00 - c10*s/p10 - c01*s/p01)
+      res[1 + seq_len(N)] = c11*(p2+dp1)/p11 + c10*(q2-dp1)/p10 + c01*(-p2-dp1)/p01 + c00*(-q2+dp1)/p00
+      res[1 + N + seq_len(N)] = c11*(p1+dp2)/p11 + c10*(q1-dp2)/p10 + c01*(-p1-dp2)/p01 + c00*(-q1+dp2)/p00
+      return(res/1000)
     }
     
     c11m = matrix(0, NG, NG); c00m = matrix(0, NG, NG)
@@ -195,13 +254,16 @@ fitModel.NetworkStructSBM = function(NetS, adja, mode = "default", optim_tries =
     n = tm[lower.tri(tm, diag = TRUE)]
     
     bestval = -Inf
+    v = rep(1:optim_tries)
     for(i in 1:optim_tries) { 
-      temp = optim(rnorm(2*length(n) + 1), fn = llfx, D = D, n=n, control = list(fnscale = -1))
+      temp = optim(sample_start(2*length(n) + 1), fn = llfx, D = D, n=n, control = list(fnscale = -1))
       if (temp$value > bestval) {
         bestval = temp$value; best = temp
       }
+      v[i] = temp$value
     }
-    return(list(res, best))
+    
+    return(list(res, best, v))
   }
   
   stop("Invalid 'mode'")
---
+++ test_code/test-generic_fitModel.R
@@ -0,0 +1,13 @@
+## For testing fitModel
+library(netcompLib)
+library(abind)
+load("../../network-comparison/netcomp-project/data/method_data/small_samp_DFcorr.Rdata")
+
+
+## For SBM version
+NetS = NetworkStruct(set_model_param(Nnodes = 100))
+genmod = NetworkModelSBM(set_model_param(Nnodes = 100))
+getNnodes(genmod)
+adja = sampleNetwork(genmod, Nobs = 2)
+optim_tries = 10
+mode = 'densitydiff'
