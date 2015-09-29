051a45de1476d91d9bbd60972397e7cad3ecaaf5
Lawrence Wang <625lawrence@gmail.com>
2015-09-29 16:51:40 -0400

    Issue #13 -- fix documentation and link for verbosity

--- netcompLib/R/documentation_placeholders.R
+++ netcompLib/R/documentation_placeholders.R
@@ -1,12 +1,11 @@
 
 
-#' \title{Output Settings}
+#' Output Settings
 #'
-#' \section{Purpose}{
+#' @section Purpose:
 #'   The output adjustment for functions in this package (especially the simulation functions) have multiple parts to it, and has a common format for all functions. 
-#' }
 #' 
-#' \section{Parameters}{
+#' @section Parameters:
 #'   \itemize{
 #'     \item verbose [logical] :: Function prints progress if TRUE. In simple settings, this may be the only function parameter
 #'     \item vbset [vector[3]-int] :: The entries of this vector corresponds to: (Note that all negative values in this vector are treated as zeros)
@@ -16,12 +15,10 @@
 #'       \item vbset[3] - The number of filler characters. This is usually incremented by 2 on each function call (so within-function-calls have output that is indented)
 #'     }
 #'   }
-#' }
 #' 
-#' \section{Programming Notes}{
+#' @section Programming Notes:
 #'   When calling a function, vbset_new = vbset + c(-1, -1, 2). This says to reduce the number of levels of output in the called function, as well as increase the indentation. 
-#' }
-#'
+#' 
 #' @return [] :: Nothing -- this function should not be called. 
 #' 
 #' @export
