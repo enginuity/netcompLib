64f60dbed85bb2c450109c675bcabfda65deb98c
Lawrence Wang <625lawrence@gmail.com>
2015-10-09 13:07:11 -0400

    Move out helper functions into a new code file

--- netcompLib/R/h_edgegroupLik.R
+++ netcompLib/R/h_edgegroupLik.R
@@ -3,45 +3,6 @@
 
 # Functions to aggregate over edge groups ---------------------------------
 
-#' Hides a random set of edges
-#' 
-#' @param adjm [matrix-int] :: Input adjacency matrix
-#' @param frac [double] :: Fraction of edges to hide
-#' 
-#' @return [matrix-int] :: Adjacency matrix with hidden edges
-#' 
-#' @export
-#' 
-hide_edges = function(adjm, frac = 0.1) {
-  ## TODO: Make this work for arrays
-  ## copied from netcompSBM
-  tm = matrix(1:(nrow(adjm)^2), nrow = nrow(adjm))
-  vals = tm[upper.tri(tm)]
-  vals = sample(vals, size = floor(length(vals) * frac))
-  adjm[vals] = NA
-  return(symmetrize_mat(adjm))
-}
-
-
-#' Symmetrizes matrix by using upper-triangular portion
-#' 
-#' @param mat [matrix] :: Input matrix
-#' 
-#' @return [matrix] :: Filled matrix (with lower triangular portion replaced by the upper triangular portion). 
-#' 
-#' @export
-#' 
-symmetrize_mat = function(mat) {
-  ## copied from netcompSBM
-  diag_mat = diag(mat)
-  mat[lower.tri(mat, diag = TRUE)] = 0
-  mat = mat + t(mat)
-  diag(mat) = diag_mat
-  return(mat)
-}
-
-
-
 
 #' Compute sufficient statstics for a single network
 #' 
@@ -162,6 +123,8 @@ reassign_edgegroup_prob = function(NetM, ids, probs) {
   
   return(NetM)
 }
+
+
 # Likelihood Functions -- based on edge groups ----------------------------
 
 
