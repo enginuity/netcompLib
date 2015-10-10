9334b17fa69f96d5ce73b80ac8ac9cc698126988
Lawrence Wang <625lawrence@gmail.com>
2015-10-10 19:53:52 -0400

    Issue #26 -- copy stuff from extractStruct to AllClasses

--- netcompLib/R/AllClasses.R
+++ netcompLib/R/AllClasses.R
@@ -394,6 +394,14 @@ NetworkStruct = function(model_params = set_model_param()) {
 #' @export
 #' 
 NetworkStructList = function(Nmodels = 10, model_params = set_model_param()) {
+#   #|----##Issue #26 -- destroy this function; replace with call to base NetworkStruct function. --Sat Oct 10 19:52:04 2015--
+#   netsl = NetworkStructList(Nmodels = 2)
+#   netsl@models[[1]] = extractStruct(NetM@m1)
+#   #|----##Issue #26 -- destroy this function; replace with call to base NetworkStruct function. --Sat Oct 10 19:52:04 2015--
+#   netsl@models[[2]] = extractStruct(NetM@m2)
+#   #|----##Issue #26 -- destroy this function; replace with call to base NetworkStruct function. --Sat Oct 10 19:52:04 2015--
+#   return(netsl)
+#   
   
   res = replicate(n = Nmodels, expr = NetworkStruct(model_params))
   netsl = new("NetworkStructList", Nnodes = model_params$Nnodes, models = res)
@@ -413,9 +421,13 @@ NetworkStructList = function(Nmodels = 10, model_params = set_model_param()) {
 #' @export
 #' 
 NetworkStructRND = function(model_params = set_model_param(), NetM = NULL) {
+#   nets = new("NetworkStructRND", Nnodes = getNnodes(NetM), counts = NetM@counts, ids = NetM@ids)
+#   return(nets)
+#   
   # Just generate a model and then lose the probability information. 
   NetM = NetworkModelRND(model_params)
   return(extractStruct(NetM))
+#|----##Issue #26 -- destroy this function; replace with call to base NetworkStruct function. --Sat Oct 10 19:52:04 2015--
 }
 
 
@@ -431,9 +443,19 @@ NetworkStructRND = function(model_params = set_model_param(), NetM = NULL) {
 #' @export
 #' 
 NetworkStructHRG = function(model_params = set_model_param(), NetM = NULL) {
+#   tr = list(prob = NetM@prob, children = NetM@children, parents = NetM@parents, nodes = getNnodes(NetM))
+#   expc = expanded_children_from_tree(tr)
+#   
+#   nets = new("NetworkStructHRG", Nnodes = getNnodes(NetM), tree_list = tr, expand = expc, 
+#              counts = sapply(expc, function(x) {length(x[[1]]) * length(x[[2]]) }))
+#   return(nets)
+#   
+#   
+#   
   # Just generate a model and then lose the probability information. 
   NetM = NetworkModelHRG(model_params)
   return(extractStruct(NetM))
+#|----##Issue #26 -- destroy this function; replace with call to base NetworkStruct function. --Sat Oct 10 19:52:04 2015--
 }
 
 
@@ -449,8 +471,36 @@ NetworkStructHRG = function(model_params = set_model_param(), NetM = NULL) {
 #' @export
 #' 
 NetworkStructSBM = function(model_params = set_model_param(), NetM = NULL) {
+#   
+#   # group assignments
+#   ga = NetM@groups
+#   NClass = length(unique(ga))
+#   
+#   counts = rep(0, times = NClass + NClass * (NClass - 1) / 2)
+#   correction = rep(0, times = NClass + NClass * (NClass - 1) / 2)
+#   
+#   expanded = list()
+#   cur = 1
+#   for(k in 1:NClass) { for(m in 1:NClass) {
+#     if (k >= m) {
+#       if (k == m) { 
+#         counts[cur] = sum(ga == k) * (sum(ga == k) - 1) / 2 
+#         correction[cur] = 2
+#       } else {
+#         counts[cur] = sum(ga == k) * sum(ga == m)
+#         correction[cur] = 1
+#       }
+#       expanded[[cur]] = list(which(ga == k), which(ga == m))
+#       cur = cur + 1
+#     }
+#   }}
+#   
+#   nets = new("NetworkStructSBM", Nnodes = getNnodes(NetM), groups = ga, counts = counts, expand = expanded, correct = correction)
+#   
+  
   # Just generate a model and then lose the probability information. 
   NetM = NetworkModelSBM(model_params)
   return(extractStruct(NetM))
+#|----##Issue #26 -- destroy this function; replace with call to base NetworkStruct function. --Sat Oct 10 19:52:04 2015--
 }
 
