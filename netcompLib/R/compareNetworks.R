ee4c0cf44e86613cc31a50b3304674967b0aab90
Lawrence Wang <625lawrence@gmail.com>
2016-01-13 21:33:38 -0500

    Issue #8 -- Add skeleton for compareNetworks function

---
+++ netcompLib/R/compareNetworks.R
@@ -0,0 +1,16 @@
+##@S This file contains the main network comparison function. 
+
+
+compareNetworks = function() {
+  ## Step 0 -- Clean / check input data
+  
+  ## Step 1 -- Hide part of the input adjacency arrays
+  
+  ## Step 2 -- Fit appropriate dyad parition to the non-hidden part of the data
+  
+  ## Step 3 -- Compute test statistic on the fitted model
+  
+  ## Step 4 -- Apply multiple comparisons methods if needed for doing multiple data splits
+  
+  
+}
