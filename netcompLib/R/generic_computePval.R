ba41a8a36c458b552890bb4b44ff95ae8f59a1cc
Lawrence Wang <625lawrence@gmail.com>
2016-02-11 16:43:56 -0500

    Issue #23 -- Prep computePval to allow code for computing p-values

--- netcompLib/R/generic_computePval.R
+++ netcompLib/R/generic_computePval.R
@@ -1,6 +1,6 @@
 ##@S Generic function that performs the hypothesis test (computes the p-value for the likelihood ratio test)
 
-setGeneric("computePval", function(NetS, adja1, adja2, Nobs, pl, mode, model_type, verbose, vbset) standardGeneric("computePval"))
+setGeneric("computePval", function(NetS, adja1, adja2, Nobs, pl, output_mode, model_type, verbose, vbset) standardGeneric("computePval"))
 
 
 #' Computes p-value for likelihood ratio test
@@ -12,7 +12,7 @@ setGeneric("computePval", function(NetS, adja1, adja2, Nobs, pl, mode, model_typ
 #' @param adja2 [matrix/array] :: Adjacency matrix/array
 #' @param Nobs [int] :: Number of network observations per class (default = 1)
 #' @param pl [list] :: Simulation/Testing parameters, set by set_sim_param
-#' @param mode [char] :: How to output results? Options are: 
+#' @param output_mode [char] :: How to output results? Options are: 
 #' \itemize{
 #' \item 'chisq' :: Only gives the asymptotically chi-square test statistic
 #' \item 'nodal' :: Outputs break down of test statistic into each vertex
@@ -33,53 +33,64 @@ setGeneric("computePval", function(NetS, adja1, adja2, Nobs, pl, mode, model_typ
 #' 
 #' @export
 #' 
-computePval = function(NetS, adja1, adja2, Nobs = 1, pl, mode = 'chisq', model_type = "default", verbose = TRUE, vbset = c(1,0,0)) {
+computePval = function(NetS, adja1, adja2, Nobs = 1, pl, output_mode = 'pval', model_type = "default", verbose = TRUE, vbset = c(1,0,0)) {
   stop("Placeholder for documentation purposes")
 }
 
 
-computePval.NetworkStruct = function(NetS, adja1, adja2, Nobs = 1, pl, mode = "chisq", model_type = "default",  verbose = TRUE, vbset = c(1,0,0)) {
+computePval.NetworkStruct = function(NetS, adja1, adja2, Nobs = 1, pl, output_mode = "pval", model_type = "default", verbose = TRUE, vbset = c(1,0,0)) {
   
-  ## TODO: [Update] fix implmenetation of parameter list; since set_sim_param has been updated. 
-  ## 'pl' not even used here... should it be? 
-  ## New implmenetation -- this can be used for all NetworkStruct (as long as its not a list)
   ## TODO: Change code to work when Nobs != 1... 
-  if (mode != "default") {
-    by_node = (mode == "nodal")
-    ## Fit on appropriate model_type
-    adj_abound = abind::abind(adja1, adja2, along = 3)
+  
+  ## Compute everything by each node
+  adj_abound = abind::abind(adja1, adja2, along = 3)
+  
+  ## Fit on appropriate model_type
+  if (model_type %in% c("default", "densitydiff")) {
+    fitN = fitModel(NetS, adj_abound, mode = model_type)
+    fit1 = fitModel(NetS, adja1); fit2 = fitModel(NetS, adja2)
     
-    if (model_type %in% c("default", "densitydiff")) {
-      fitN = fitModel(NetS, adj_abound, mode = model_type)
-      fit1 = fitModel(NetS, adja1); fit2 = fitModel(NetS, adja2)
-      
-      llN = computeLik(fitN, adj_abound, by_node = TRUE)$bynode
-      llA = computeLik(fit1, adja1, by_node = TRUE)$bynode + 
-        computeLik(fit2, adja2, by_node = TRUE)$bynode
-    } else if (model_type == "correlated") {
-      fitN = fitModel(NetS, adj_abound, mode = "corr-global-null")
-      fitA = fitModel(NetS, adj_abound, mode = "corr-global")
-      
-      llN = computeLik(fitN, adj_abound, by_node = by_node)$bynode
-      llA = computeLik(fitA, adj_abound, by_node = by_node)$bynode
-    }
+    llN = computeLik(fitN, adj_abound, by_node = TRUE)$bynode
+    llA = computeLik(fit1, adja1, by_node = TRUE)$bynode + 
+      computeLik(fit2, adja2, by_node = TRUE)$bynode
+  } else if (model_type == "correlated") {
+    fitN = fitModel(NetS, adj_abound, mode = "corr-global-null")
+    fitA = fitModel(NetS, adj_abound, mode = "corr-global")
     
-    chisq = sum(-2 * (llN - llA)) 
+    llN = computeLik(fitN, adj_abound, by_node = TRUE)$bynode
+    llA = computeLik(fitA, adj_abound, by_node = TRUE)$bynode
+  }
+  
+  ## Compute chi-square test statstic
+  chisqByNode = -2 * (llN - llA)
+  chisq = sum(chisqByNode) 
+  
+  ## Compute p-value if desired 
+  if (output_mode == "chisq") {
+    return(chisq)
+  } else if (output_mode %in% c("pval", "nodal")) {
+    
+    ## TODO: [Update] fix implmenetation of parameter list; since set_sim_param has been updated. 
+    ## 'pl' not even used here... should it be? -- yes, when computing p-values. 
+    ## New implmenetation -- this can be used for all NetworkStruct (as long as its not a list)
+    
+    
+    
     ## TODO: Implement pvals case
-    if (mode == "chisq") {
-      return(chisq)
+    pvals = 0 ## IMPLEMENT THIS PROPERLY
+    if (output_mode == "nodal") {
+      
+      return(list(chisq = chisq, pvals = pvals, nodecontrib = chisqByNode))
     } else {
-      if (by_node) {
-        return(list(chisq = chisq, pvals = "NOT IMPLEMENTED", nodecontrib = -2 * (llN - llA)))
-      }
+      return(pvals)
     }
-    
-    return(-2*(llN - llA))
+  } else {
+    stop("Invalid output_mode")
   }
 }
 
 
-computePval.NetworkStructList = function(NetS, adja1, adja2, Nobs = 1, pl, mode = "chisq", model_type = "default", verbose = TRUE, vbset = c(1,0,0)) {
+computePval.NetworkStructList = function(NetS, adja1, adja2, Nobs = 1, pl, output_mode = "chisq", model_type = "default", verbose = TRUE, vbset = c(1,0,0)) {
   if (verbose & vbset[1] > 0) { 
     cat("\n", stringr::str_pad(string = "", width = vbset[3], pad = "-"), date(), "-- Fitting on", length(NetS@models), "random structures")
   }
@@ -87,7 +98,7 @@ computePval.NetworkStructList = function(NetS, adja1, adja2, Nobs = 1, pl, mode 
   ## Call appropriate computePval for each individual NetworkStruct
   res = lapply(NetS@models, function(x) { 
     if (verbose & vbset[1] > 0 & vbset[2] > 0) { cat(".") }
-    computePval(x, adja1, adja2, Nobs, pl, mode, model_type, verbose, vs_new) 
+    computePval(x, adja1, adja2, Nobs, pl, output_mode, model_type, verbose, vs_new) 
   } )
   return(res)
 }
