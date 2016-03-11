a52cba19972ff261a9d03e085e1434bcc145c455
Lawrence Wang <625lawrence@gmail.com>
2016-03-10 21:45:05 -0500

    Edit complete test code

--- test_code/test-complete.R
+++ test_code/test-complete.R
@@ -10,12 +10,21 @@ adj1 = matrix(c(0,1,1,0,0,
                 0,1,0,1,0), nrow = 5)
 adj2 = matrix(c(0,0,1,0,0,
                 0,0,0,0,1,
-                1,0,0,1,1,
+                1,0,0,1,0,
                 0,0,1,0,1,
-                0,1,1,1,0), nrow = 5)
+                0,1,0,1,0), nrow = 5)
 
 NetS = NetworkStruct(set_model_param(Nnodes = 5, block_assign = c(1,1,1,2,2)))
 m1 = fitModel(NetS, adj1)
 m2 = fitModel(NetS, adj2)
+
+abs(m1@probmat - matrix(c(2/3, 3/6, 3/6, 1/1), nrow = 2))
+abs(m2@probmat - matrix(c(1/3, 2/6, 2/6, 1/1), nrow = 2))
 mc = fitModel(NetS, abind(adj1, adj2, along = 3))
-computePval(NetS, adj1, adj2, pl = set_sim_param(cc_adj = 0, thres_ignore = 0))
+abs(mc@probmat - matrix(c(3/6, 5/12, 5/12, 2/2), nrow = 2))
+
+computePval(NetS, adj1, adj2, pl = set_sim_param(cc_adj = 0, thres_ignore = 0), output_mode = "chisq")
+
+
+
+
